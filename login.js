5a9b8ee816b83e168007b88ed720829ac0762804
Linqi <sunlinqi110@gmail.com>
2021-05-08 22:07:51 +0800

    add a login window

---
+++ login.js
@@ -0,0 +1,9 @@
+function Show() {
+	document.getElementById('shade').classList.remove('hide');
+	document.getElementById('modal').classList.remove('hide');
+}
+
+function Hide() {
+	document.getElementById('shade').classList.add('hide');
+	document.getElementById('modal').classList.add('hide');
+}
